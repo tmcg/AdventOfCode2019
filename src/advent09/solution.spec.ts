5a9de2215d26f7e063b00893727d3b7204ccddbf
Tony McGee <tmcgee.au@gmail.com>
2019-12-09 20:42:13 +1000

    Day 9 completed

---
+++ src/advent09/solution.spec.ts
@@ -0,0 +1,15 @@
+
+import solution from './solution';
+import { expect } from 'chai';
+import 'mocha';
+
+describe(`Advent of Code Day ${solution.dayNumber}`, () => {
+
+  it('should solve part 1', () => {
+    expect(solution.solvePart1()).to.equal('2377080455');
+  });
+
+  it('should solve part 2', () => {
+    expect(solution.solvePart2()).to.equal('74917');
+  });
+});
